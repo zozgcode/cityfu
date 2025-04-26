dbcd0e33302fbe4876fc8941d8ae6db39a927f76
sunshine-web-coder <webdev4gaming@gmail.com>
2025-04-26 11:49:15 +0100

    Fdf

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -29,13 +29,13 @@ export const mockAccounts: Account[] = [
         dateTime: 'February 3, 2025',
         description: 'AirBnB Hawaii debit',
         status: 'Success',
-        amount_usd: -4850.00
+        amount_usd: -4850.0
       },
       {
         dateTime: 'February 2, 2025',
         description: 'Debit from United airlines',
         status: 'Success',
-        amount_usd: -1800.00
+        amount_usd: -1800.0
       },
       {
         dateTime: 'January 28, 2025',
@@ -47,31 +47,31 @@ export const mockAccounts: Account[] = [
         dateTime: 'January 26, 2025',
         description: 'Withdrawal chk#229152',
         status: 'Success',
-        amount_usd: -250120.00
+        amount_usd: -250120.0
       },
       {
         dateTime: 'December 29, 2024',
         description: 'Debit from Walmart',
         status: 'Success',
-        amount_usd: -1200.00
+        amount_usd: -1200.0
       },
       {
         dateTime: 'December 17, 2024',
         description: 'Teller Withdrawal',
         status: 'Success',
-        amount_usd: -50000.00
+        amount_usd: -50000.0
       },
       {
         dateTime: 'December 15, 2024',
         description: 'Credit from fidelity investment',
         status: 'Success',
-        amount_usd: 980000.00
+        amount_usd: 980000.0
       },
       {
         dateTime: 'December 13, 2024',
         description: 'Transfer to Joe Harden',
         status: 'Success',
-        amount_usd: -320000.00
+        amount_usd: -320000.0
       },
       {
         dateTime: 'August 5, 2024',
@@ -139,7 +139,7 @@ export const mockAccounts: Account[] = [
       account_type: 'Checking',
       account_name: true,
       account_number: true,
-      balance_usd: 4560000.00
+      balance_usd: 4560000.0
     },
     transaction_mgs_code: {
       transaction_text_msg: 'To continue this transaction, please enter the code sent to you',
@@ -152,41 +152,41 @@ export const mockAccounts: Account[] = [
         dateTime: 'January 10, 2025',
         description: 'Transfer to Kyla Micheal',
         status: 'Success',
-        amount_usd: -20000.00
+        amount_usd: -20000.0
       },
       {
         dateTime: 'October 28, 2024',
         description: 'Transfer to The part school of Baltimore',
         status: 'Success',
-        amount_usd: -52000.00
+        amount_usd: -52000.0
       },
       {
         dateTime: 'October 25, 2024',
         description: 'Transfer from Emily Rose',
         status: 'Success',
-        amount_usd: 3000.0,
+        amount_usd: 3000.0
       },
       {
         dateTime: 'October 25, 2024',
         description: 'Transfer to Starbucks',
         status: 'Success',
-        amount_usd: -35.99,
+        amount_usd: -35.99
       },
       {
         dateTime: 'October 25, 2024',
         description: 'Transfer to Michael Daniel',
         status: 'Success',
-        amount_usd: -250.0,
+        amount_usd: -250.0
       },
       {
         dateTime: 'October 24, 2024',
         description: 'Transfer from Olivia Marir',
         status: 'Success',
-        amount_usd: 1870.0,
-      },
+        amount_usd: 1870.0
+      }
     ]
   },
-   {
+  {
     account_id: 'ACC003',
     holder: {
       firstName: 'Kijan Coryaba',
@@ -201,7 +201,7 @@ export const mockAccounts: Account[] = [
       account_type: 'Checking',
       account_name: true,
       account_number: true,
-      balance_usd: 4650000.00
+      balance_usd: 4650000.0
     },
     transaction_mgs_code: {
       transaction_text_msg: 'To continue this transaction, please enter the code sent to you',
@@ -214,31 +214,31 @@ export const mockAccounts: Account[] = [
         dateTime: 'March 31, 2025',
         description: 'Postpaid - XXXXXX2526',
         status: 'Success',
-        amount_usd: 60000.00
+        amount_usd: 60000.0
       },
       {
         dateTime: 'March 31, 2025',
         description: 'Discover bills - XXXXXX3838',
         status: 'Success',
-        amount_usd: 30000.00
+        amount_usd: 30000.0
       },
       {
         dateTime: 'January 10, 2025',
         description: 'Transfer to Kyler Micheal - XXXXXX9469',
         status: 'Success',
-        amount_usd: -20000.00
+        amount_usd: -20000.0
       },
       {
         dateTime: 'October 28, 2024',
         description: 'Transfer to The part school of Baltimore - XXXXXX8468',
         status: 'Success',
-        amount_usd: -52000.00
+        amount_usd: -52000.0
       },
       {
         dateTime: 'October 25, 2024',
         description: 'Transfer from Emily Rose - XXXXXX1277',
         status: 'Success',
-        amount_usd: 3000.00
+        amount_usd: 3000.0
       },
       {
         dateTime: 'October 25, 2024',
@@ -250,14 +250,64 @@ export const mockAccounts: Account[] = [
         dateTime: 'October 25, 2024',
         description: 'Transfer to Micheal Daniel - XXXXXX7389',
         status: 'Success',
-        amount_usd: -250.00
+        amount_usd: -250.0
       },
       {
         dateTime: 'October 24, 2024',
         description: 'Transfer from Olivia Marir - XXXXXX7389',
         status: 'Success',
-        amount_usd: 1870.00
-      },
+        amount_usd: 1870.0
+      }
     ]
   },
+  {
+    account_id: 'ACC004',
+    holder: {
+      firstName: 'Teresa Samaroo',
+      lastName: '',
+      mobileNumber: '***-***-***',
+      email: 'tdsamaro2@gmail.com ',
+      createdOn: '4/26/2025', // m/d/y
+      username: 'Tsamaroo',
+      password: 'Samaroocr25'
+    },
+    bank_details: {
+      account_type: 'Checking',
+      account_name: true,
+      account_number: true,
+      balance_usd: 150020.0
+    },
+    transaction_mgs_code: {
+      transaction_text_msg: 'To continue this transaction, please enter the code sent to you',
+      transaction_code: '0317',
+      lastStepText: 'Your account is on hold. You cannot make transactions right now. Please contact our customer service team for assistance.',
+      wireDate: true
+    },
+    transaction_history: [
+      {
+        dateTime: 'April 24, 2025',
+        description: 'Credit from Chase rice management - XXXXXX2526',
+        status: 'Success',
+        amount_usd: 150000.0
+      },
+      {
+        dateTime: 'April 24, 2025',
+        description: 'Vat Tax - XXXXXX4838',
+        status: 'Success',
+        amount_usd: -5.0
+      },
+      {
+        dateTime: 'April 24, 2025',
+        description: 'Charges - XXXXXX4838',
+        status: 'Success',
+        amount_usd: -5.0
+      },
+      {
+        dateTime: 'April 24, 2025',
+        description: 'Opening Balance - XXXXXX4838',
+        status: 'Success',
+        amount_usd: 10.0
+      },
+    ]
+  }
 ];
